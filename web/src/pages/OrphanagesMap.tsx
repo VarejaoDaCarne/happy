68b0065ea11204b640c65ce4852103871caf7e0b
Gabriel <gabriel.al.rio@gmail.com>
2020-10-13 12:03:47 -0300

    minor errors corrected

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import { Map, TileLayer } from 'react-leaflet'
-
 import 'leaflet/dist/leaflet.css'
-
 import mapMarkerImg from '../images/map-marker.svg'
-
 import '../styles/pages/orphanages-map.css'
 
 function OrphanagesMap() {
@@ -16,8 +13,8 @@ function OrphanagesMap() {
                 <header>
                     <img src={mapMarkerImg} alt="Happy"/>
 
-                    <h2>Escolha um orfanato no map</h2>
-                    <p>Mutias crianças estão esperando a sua visita :)</p>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
 
                 <footer>
@@ -27,11 +24,11 @@ function OrphanagesMap() {
             </aside>
 
             <Map
-                center={[-23.614687, -46.4587521]}
+                center={[-23.6143721, -46.459095]}
                 zoom={11}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer url="https://a.title.openstreetmap.org/{z}/{x}/{y}.png" />
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             </Map>
             ,
             11
